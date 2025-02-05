71ebd59491a26a111acfc31e4752abb998891752
wacksky <335496605@qq.com>
2025-02-05 17:01:20 +0800

    idea第二次

--- git-test-project/src/com/itheima/git/First.java
+++ git-test-project/src/com/itheima/git/First.java
@@ -8,4 +8,9 @@ public class First {
         System.out.println("git启动了");
         System.out.println("git启动了");
     }
+    public  void test(){
+        int i = 0;
+        i+=10;
+        System.out.println(i);
+    }
 }
